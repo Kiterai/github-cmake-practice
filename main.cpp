5c527f2efbf8f0392b4ee56f50ae1dd5ea60dd8b
kiterai <kiterai@foolslab.net>
2024-07-27 13:25:44 +0900

    vulkan

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <lib.hpp>
+#include <vulkan/vulkan.hpp>
 
 int main() {
     hello();
+
+    vk::InstanceCreateInfo instCreateInfo;
+    vk::Instance instance = vk::createInstance(instCreateInfo);
+
+    instance.destroy();
 }
